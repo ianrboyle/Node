262d54addcbf682b35799e644fa8c236a40a473c
Ian Boyle <irboyle21@gmail.com>
2022-02-17 14:49:10 -0500

    adds input button where input is then displayed in separate route

--- practice/routes.js
+++ practice/routes.js
@@ -7,17 +7,25 @@ const routeHandler = (req, res) => {
     res.write("<html>");
     res.write("<head><title>Login</title></head>");
     res.write(
-      '<body><h1>UserName: </h1><form action="/users" method="POST"><input type="text" name="username"><button type="submit">Submit</button></body>'
+      '<body><h1>UserName: </h1><form action="/users" method="POST"><input type="text" name="username"><button type="submit">Submit</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
   if (req.url === "/users" && method === "POST") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Welcome</title></head>");
-    res.write("<body><h1>Welcome</h1></body>");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<head><title>Welcome</title></head>");
+      res.write(`<body><h1>Welcome ${parsedBody.split("=")[1]}</h1></body>`);
+      return res.end();
+    });
   }
 };
 
